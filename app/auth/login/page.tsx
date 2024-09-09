55057950ff1317c881270b387cc270d800c8ddb7
Tung Developer <tientung03.nttvn@gmail.com>
2024-09-09 09:42:39 +0700

    Done setup project base

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,13 @@
+import LoginForm from '@/components/auth/login-form'
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>
+      Login page
+      <LoginForm/>
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ components/auth/card-wrapprt.tsx
@@ -0,0 +1,12 @@
+import { Children } from "react";
+
+interface CardWarpperProps{
+    children:React.ReactNode;
+    headderLabel:string;
+    backButtonLabel:string;
+    backButtonHref:string;
+    showSocial?:boolean;
+}
+export const CardWarpper=({Children,headerLabel,backButton,backButtonHref,showSocial}:CardWarpperProps)=>{
+    
+}

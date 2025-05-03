49595622823b482be29b2e3d0c590003bae3e352
Tung03 <tientung03.nttvn@gmail.com>
2025-05-04 01:36:02 +0700

    fix loading status content in the table data

--- components/LoadingWrapper.tsx
+++ components/LoadingWrapper.tsx
@@ -12,3 +12,5 @@ const LoadingWrapper = ({ loading, children }: { loading: boolean; children: Rea
   return <>{children}</>;
 };
 export default LoadingWrapper;
+const [servicePatientNotInTreatmentSessions,setServicePatientNotInTreatmentSessions]=useState<ServicePatient[]>([]);
+const [servicePatientTreatmentSessions,setServicePatientTreatmentSessions]=useState<ServicePatient[]>([]);
